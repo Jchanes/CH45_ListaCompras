d3df7ac31a7e048e7f04f88694de78ef122538b9
JM Chanes <qwertyuiop.xd1@gmail.com>
2024-09-03 13:03:43 -0600

    modified:   README.md 	se añadio el paso 8
    	modified:   index.html
    	se añadio la tabla
    
    	modified:   js/main.js
    	se añadieron las constantes de contador, total en
    	productos, resumen, asi como la funcion del
    	localstorage para almacenar los items asi como sus precios

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ const txtNombre = document.getElementById("Name");
 const txtNumber = document.getElementById("Number");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+const contadorProductos = document.getElementById("contadorProductos");
+const productosTotal = document.getElementById("productosTotal");
+const precioTotal = document.getElementById("precioTotal");
+
+
+//bandera, al ser true permite agregar los datos a la tabla
+let isValid = true;
+let contador = 0;
+let precio = 0;
+let costoTotal = 0;
+let totalEnProductos = 0;
 
     function validarCantidad(){
         if (txtNumber.value.length==0){
@@ -18,9 +31,11 @@ const alertValidacionesTexto = document.getElementById("alertValidacionesTexto")
         }//<0
 
         return true;
-
-
     }
+
+    function getPrecio(){
+        return Math.round((Math.random()*10000))/100;
+    }//getPrecio
 
 
 btnAgregar.addEventListener("click", function(event){
@@ -29,11 +44,15 @@ btnAgregar.addEventListener("click", function(event){
         txtNumber.style.border= "";
         alertValidacionesTexto.innerHTML= "";
         alertValidaciones.style.display= "none";
+        isValid=true;
+
 //Validar el nombre del producto
     if(txtNombre.value.length<3){
         txtNombre.style.border = "solid red medium";
         alertValidacionesTexto.innerHTML= "El <strong>Nombre</strong> no es correcto </br>";
         alertValidaciones.style.display= "block";
+        isValid=false;
+
     }//if lenght
 
     // validar cantidad
@@ -41,7 +60,39 @@ btnAgregar.addEventListener("click", function(event){
         txtNumber.style.border="solid red medium";
         alertValidacionesTexto.innerHTML+="La <strong>Cantidad</strong> no es correcto </br>"
         alertValidaciones.style.display="block"
+        isValid=false;
     }
+
+    if (isValid) {
+        contador ++;
+        precio = getPrecio();
+        let row = `<tr>
+                        <td>${contador}</td>
+                        <td>${txtNombre.value}</td>
+                        <td>${txtNumber.value}</td>
+                        <td>${precio}</td>
+        </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend",row);
+
+        costoTotal += precio * Number(txtNumber.value);
+        totalEnProductos += Number(txtNumber.value);
+        contadorProductos.innerText = contador;
+        productosTotal.innerText = totalEnProductos;
+        precioTotal.innerText = "$ " + costoTotal;
+        
+        localStorage.setItem("contador", contador);
+        localStorage.setItem("totalEnProductos", totalEnProductos);
+        localStorage.setItem("costoTotal", costoTotal);
+
+
+
+        txtNombre.value="";
+        txtNumber.value="";
+        txtNombre.focus();
+
+    }//isValid se agregan los elementos y se genera la tabla
+
+
 
 })//btnAgregar.addeventlistener
 
@@ -52,3 +103,19 @@ txtNombre.addEventListener("blur", function(event){
 txtNumber.addEventListener("blur", function(event){
     txtNumber.value = txtNumber.value.trim();
 })//txtNumber.addEventListener
+
+window.addEventListener("load", function(){
+    if (this.localStorage.getItem("contador") != null) {
+        contador = Number(this.localStorage.getItem("contador"));
+    }//!=null
+
+    if (this.localStorage.getItem("totalEnProductos") != null) {
+        totalEnProductos = Number(this.localStorage.getItem("totalEnProductos"));
+    }//!=null
+
+    if (this.localStorage.getItem("costoTotal") != null) {
+        costoTotal = Number(this.localStorage.getItem("contador"));
+    }//!=null
+
+    
+})
